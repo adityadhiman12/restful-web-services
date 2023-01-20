a0b4124c20a2df90ba475ebfcde5332dc692d690
Aditya Dhiman <adityadhiman12@gmail.com>
2023-01-21 00:53:18 +0530

    another way to use GET request via get mapping

--- src/main/java/com/aditya/rest/webservices/restfulwebservices/hellworld/HelloWorldController.java
+++ src/main/java/com/aditya/rest/webservices/restfulwebservices/hellworld/HelloWorldController.java
@@ -1,12 +1,11 @@
 package com.aditya.rest.webservices.restfulwebservices.hellworld;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
-    @RequestMapping(method = RequestMethod.GET, path="/hello-world")
+    @GetMapping(path="/hello-world")
     public String HelloWorld () {
         return "Hello World";
     }
