efb29865b4a6fafe7ea2291961fe210411846838
Aditya Dhiman <adityadhiman12@gmail.com>
2023-01-21 00:59:30 +0530

    returning a hello world bean

--- src/main/java/com/aditya/rest/webservices/restfulwebservices/hellworld/HelloWorldController.java
+++ src/main/java/com/aditya/rest/webservices/restfulwebservices/hellworld/HelloWorldController.java
@@ -9,4 +9,9 @@ public class HelloWorldController {
     public String HelloWorld () {
         return "Hello World";
     }
+
+    @GetMapping(path = "/hello-world-bean")
+    public HelloWorldBean helloWorldBean() {
+        return new HelloWorldBean("Hello World bean");
+    }
 }
