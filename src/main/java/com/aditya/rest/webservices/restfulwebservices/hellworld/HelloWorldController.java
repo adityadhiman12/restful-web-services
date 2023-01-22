666f52c3dfae61bceafcfdec50d6aebab113aed9
Aditya Dhiman <adityadhiman12@gmail.com>
2023-01-23 00:34:32 +0530

    adding i18n config

--- src/main/java/com/aditya/rest/webservices/restfulwebservices/hellworld/HelloWorldController.java
+++ src/main/java/com/aditya/rest/webservices/restfulwebservices/hellworld/HelloWorldController.java
@@ -1,14 +1,29 @@
 package com.aditya.rest.webservices.restfulwebservices.hellworld;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 @RestController
 public class HelloWorldController {
+    private MessageSource messageSource;
+    public HelloWorldController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
     @GetMapping(path="/hello-world")
     public String HelloWorld () {
         return "Hello World";
+    }
+
+    @GetMapping(path="/hello-world-i18n")
+    public String HelloWorldi18n () {
+        Locale locale = LocaleContextHolder.getLocale();
+        messageSource.getMessage("good.morning.message", null,"Default Message", locale);
+        return "Hello World V2";
     }
 
     @GetMapping(path = "/hello-world-bean")
